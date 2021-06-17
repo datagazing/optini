add36ae282b9272ab60fab43096fea4d354699a2
Brendan Strejcek <brendan@datagazing.com>
2021-06-17 14:29:13 -0400

    remove unparsed global object (use opt._unparsed instead)

--- optini/__init__.py
+++ optini/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
 __version__ = '0.3.0'
 
-from .optini import Config, opt, spec, unparsed, section
+from .optini import Config, opt, spec, section
