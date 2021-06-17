d2124103cb6ad4d8c675f9555ac958347dbe2d33
Brendan Strejcek <brendan@datagazing.com>
2021-06-17 15:58:29 -0400

    Bump version: 0.4.0 → 0.4.1

--- optini/__init__.py
+++ optini/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 from .optini import Config, opt, spec, section # noqa F401
